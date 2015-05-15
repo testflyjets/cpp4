96baaa6f1b3e96c263f25c0995a7487b0b3d47df
Chris McCann <testflyjets@gmail.com>
2015-05-14 18:38:15 -0700

    Implements skeleton methods

--- hw5/auto_ptr.h
+++ hw5/auto_ptr.h
@@ -1,21 +1,29 @@
-template <typename X>
-class auto_ptr
-{
-public:
-// Constructors
-explicit auto_ptr (X * = 0) throw();
-auto_ptr(auto_ptr &) throw();
-template <typename Y> auto_ptr(auto_ptr<Y> &) throw();
-// Destructor
-~auto_ptr() throw();
-// Access
-X *get() const throw();
-X &operator*() const throw();
-X *operator->() const throw();
-// Assignment
-auto_ptr &operator=(auto_ptr &) throw();
-template <typename Y> auto_ptr &operator=(auto_ptr<Y> &) throw();
-// Release & Reset
-X *release() throw();
-void reset(X * = 0) throw();
-};
+namespace ChrisMcCann { namespace hw5 {
+
+   template <typename X>
+   class auto_ptr
+   {
+   public:
+      // Constructors
+      explicit auto_ptr(X * = 0) throw();
+      auto_ptr(auto_ptr &) throw();
+      template <typename Y> auto_ptr(auto_ptr<Y> &) throw();
+
+      // Destructor
+      ~auto_ptr() throw();
+
+      // Access
+      X *get() const throw();
+      X &operator*() const throw();
+      X *operator->() const throw();
+
+      // Assignment
+      auto_ptr &operator=(auto_ptr &) throw();
+      template <typename Y> auto_ptr &operator=(auto_ptr<Y> &) throw();
+
+      // Release & Reset
+      X *release() throw();
+      void reset(X * = 0) throw();
+   };
+
+}}
