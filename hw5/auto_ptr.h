a2f160af429d93d19e8301ef07f30157d2d4a744
Chris McCann <testflyjets@gmail.com>
2015-05-17 20:18:54 -0700

    Implements auto_ptr tests for hw5

--- hw5/auto_ptr.h
+++ hw5/auto_ptr.h
@@ -1,3 +1,19 @@
+/* Christopher C. McCann, U05484743
+ * testflyjets@gmail.com
+ * C/C++ Programming 4, Section ID 106647, Ray Mitchell III
+ * May 14, 2015
+ * auto_ptr.h
+ * Windows 8.1 Enterprise 64-bit
+ * Microsoft Visual Studio 2013 Express for Desktop
+ *
+ * The header file that defines an implementation of 
+ * the auto_ptr class.
+ * 
+ */
+
+#ifndef CHRISMCCANN_HW5_AUTOPTR_H
+#define CHRISMCCANN_HW5_AUTOPTR_H
+
 namespace ChrisMcCann { namespace hw5 {
 
    template <typename X>
@@ -24,6 +40,93 @@ namespace ChrisMcCann { namespace hw5 {
       // Release & Reset
       X *release() throw();
       void reset(X * = 0) throw();
+
+   private:
+      X *ap;
    };
 
+template <typename X>
+auto_ptr<X>::auto_ptr(X *ptr) throw()
+: ap(ptr)
+{
+}
+
+template <typename X>
+auto_ptr<X>::auto_ptr(auto_ptr &rhs)
+: ap(rhs.release())
+{
+}
+
+template <typename X>
+template <typename Y>
+auto_ptr<X>::auto_ptr(auto_ptr<Y> &rhs)
+: ap(rhs.release())
+{
+}
+
+template <typename X>
+auto_ptr<X>::~auto_ptr()
+{
+   delete ap;
+}
+
+template <typename X>
+X *
+auto_ptr<X>::get() const
+{
+   return ap;
+}
+
+template <typename X>
+X &
+auto_ptr<X>::operator*() const
+{
+   return *ap;
+}
+
+template <typename X>
+X *
+auto_ptr<X>::operator->() const
+{
+   return ap;
+}
+
+template <typename X>
+auto_ptr<X> &
+auto_ptr<X>::operator=(auto_ptr<X> &rhs)
+{
+   reset(rhs.release());
+   return *this;
+}
+
+template <typename X>
+template <typename Y> 
+auto_ptr<X> &
+auto_ptr<X>::operator=(auto_ptr<Y> &rhs)
+{
+   reset(rhs.release());
+   return *this;
+}
+
+template <typename X>
+X *
+auto_ptr<X>::release()
+{
+   X* tmp(ap);
+   ap = 0;
+   return tmp;
+}
+
+template <typename X>
+void
+auto_ptr<X>::reset(X *ptr)
+{
+   if (ap != ptr) {
+      delete ap;
+      ap = ptr;
+   }
+}
+
 }}
+
+#endif
--- hw5/hw5.cpp
+++ hw5/hw5.cpp
@@ -1,7 +1,175 @@
+/* Christopher C. McCann, U05484743
+ * testflyjets@gmail.com
+ * C/C++ Programming 4, Section ID 106647, Ray Mitchell III
+ * May 14, 2015
+ * hw5.cpp
+ * Windows 8.1 Enterprise 64-bit
+ * Microsoft Visual Studio 2013 Express for Desktop
+ *
+ * A test program for an implementation of the auto_ptr
+ * class.
+ * 
+ */
+#include <string>
 
 #include "auto_ptr.h"
+using namespace ChrisMcCann::hw5;
 
 #include "UnitTest++.h"
+
+class TestClass
+{
+public:
+   TestClass(int i = 0) : testInt(i)
+   {
+   }
+
+   ~TestClass()
+   {
+   }
+
+   void setInt(int i)
+   {
+      testInt = i;
+   }
+
+   int getInt()
+   {
+      return testInt;
+   }
+
+private:
+   int testInt;
+};
+
+class DerivedTestClass : public TestClass
+{
+
+};
+
+TEST(DefaultConstructor)
+{
+   auto_ptr<TestClass> ap;
+   
+   CHECK(ap.get() == nullptr);
+}
+
+TEST(Constructor)
+{
+   auto_ptr<TestClass> ap(new TestClass);
+
+   CHECK(ap.get() != nullptr);
+}
+
+TEST(CopyConstructor)
+{  
+   TestClass *ptr = new TestClass(42);
+   auto_ptr<TestClass> ap(ptr);
+
+   auto_ptr<TestClass> apCopy(ap);
+
+   CHECK(ap.get() == nullptr);
+   CHECK(apCopy.get() == ptr);
+}
+
+TEST(CopyConstructorDerivedClass)
+{
+   auto_ptr<DerivedTestClass> rhs(new DerivedTestClass);
+   auto_ptr<TestClass> apBase(rhs);
+
+   CHECK(apBase->getInt() == 0);
+}
+
+TEST(Destructor)
+{
+   TestClass *ptr = new TestClass;
+   auto_ptr<TestClass> ap(ptr);
+   
+   //ap.~auto_ptr<TestClass>();
+}
+
+TEST(GetAutoPtr)
+{
+   TestClass *ptr = new TestClass(42);
+   auto_ptr<TestClass> ap(ptr);
+
+   CHECK(ap.get() == ptr);
+}
+
+TEST(DereferenceAutoPtr)
+{
+   auto_ptr<TestClass> ap(new TestClass(42));
+
+   CHECK((*ap).getInt() == 42);
+}
+
+
+TEST(AutoPtrArrowOperator)
+{
+   auto_ptr<TestClass> ap(new TestClass(42));
+
+   CHECK(ap->getInt() == 42);
+}
+
+TEST(AssignmentOperator)
+{
+   auto_ptr<TestClass> ap(new TestClass(1));
+   auto_ptr<TestClass> rhs(new TestClass(2));
+
+   CHECK(ap->getInt() == 1);
+   ap = rhs;
+   CHECK(ap->getInt() == 2);
+}
+
+TEST(DerivedClassAssignmentOperator)
+{
+   auto_ptr<TestClass> ap(new TestClass(42));
+   auto_ptr<DerivedTestClass> rhs(new DerivedTestClass);
+
+   CHECK(ap->getInt() == 42);
+   ap = rhs;
+   CHECK(ap->getInt() == 0);
+}
+
+TEST(ReleaseAutoPtr)
+{
+   auto_ptr<TestClass> ap(new TestClass(42));
+   TestClass *newPtr = ap.release();
+
+   CHECK(ap.get() == nullptr);
+   CHECK(newPtr->getInt() == 42);
+}
+
+TEST(DefaultResetAutoPtr)
+{
+   auto_ptr<TestClass> ap(new TestClass(1));
+   
+   ap.reset();
+
+   CHECK(ap.get() == nullptr);
+}
+
+TEST(ResetAutoPtr)
+{
+   TestClass *ptr1 = new TestClass(1);
+   auto_ptr<TestClass> ap(ptr1);
+
+   TestClass *ptr2 = new TestClass(2);
+   
+   ap.reset(ptr2);
+
+   CHECK(ap->getInt() == 2);
+}
+
+TEST(ResetPreventSelfAssignment)
+{
+   TestClass *ptr1 = new TestClass(1);
+   auto_ptr<TestClass> ap(ptr1);
+   
+   ap.reset(ptr1);
+
+   CHECK(ap.get() == ptr1);
+}
 
 int main() {
    return UnitTest::RunAllTests();
